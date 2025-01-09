e196768236d54ad69f7214fce62c9a5be153f8ca
fahmidakm99 <93675075+fahmidakm99@users.noreply.github.com>
2025-01-09 13:53:50 +0530

    Update all-recipes-routing.module.ts

--- src/app/all-recipe/all-recipes-routing.module.ts
+++ src/app/all-recipe/all-recipes-routing.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AllRecipesPages } from './all-recipes.page';
 
-
-
-
 const routes: Routes = [
   {
     path: '',
