3be5f88b8dbc24d79f14c97107f4850dd7421aad
fahmidakm99 <93675075+fahmidakm99@users.noreply.github.com>
2025-02-24 14:44:34 +0530

    Update tabs-routing.module.ts

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -1,3 +1,47 @@
+// import { NgModule } from '@angular/core';
+// import { Routes, RouterModule } from '@angular/router';
+// import { TabsPage } from './tabs.page';
+
+// const routes: Routes = [
+//   {
+//     path: '',
+//     component: TabsPage,
+//     children: [
+//       {
+//         path: 'home',
+//         loadChildren: () => import('../home/home.module').then(m => m.HomePageModule),
+//       },
+//       {
+//         path: 'favorites',
+//         loadChildren: () => import('../favorites/favorites.module').then(m => m.FavoritesPageModule),
+//       },
+//       {
+//         path: 'categories',
+//         loadChildren: () => import('../categories/categories.module').then(m => m.CategoriesPageModule),
+//       },
+//       {
+//         path: 'shoppingList',
+//         loadChildren: () => import('../shoppingList/shoppingList.module').then(m => m.ShoppingListPageModule),
+//       },
+//       {
+//         path: '',
+//         redirectTo: 'home',
+//         pathMatch: 'full',
+//       },
+//     ],
+//   },
+//   {
+//     path: '',
+//     redirectTo: '/tabs/home',
+//     pathMatch: 'full',
+//   },
+// ];
+
+// @NgModule({
+//   imports: [RouterModule.forChild(routes)],
+//   exports: [RouterModule],
+// })
+// export class TabsPageRoutingModule {}
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TabsPage } from './tabs.page';
@@ -25,16 +69,11 @@ const routes: Routes = [
       },
       {
         path: '',
-        redirectTo: 'home',
+        redirectTo: '/tabs/home',  // ✅ This should be the only default redirect inside tabs
         pathMatch: 'full',
       },
     ],
-  },
-  {
-    path: '',
-    redirectTo: '/tabs/home',
-    pathMatch: 'full',
-  },
+  }
 ];
 
 @NgModule({
@@ -42,59 +81,3 @@ const routes: Routes = [
   exports: [RouterModule],
 })
 export class TabsPageRoutingModule {}
-
-// import { NgModule } from '@angular/core';
-// import { RouterModule, Routes } from '@angular/router';
-// import { TabsPage } from './tabs.page';
-
-// const routes: Routes = [
-//   {
-//     path: 'tabs',
-//     component: TabsPage,
-//     children: [
-//       {
-//         path: 'home',
-//         loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
-//       },
-//       {
-//         path: 'favorites',
-//         loadChildren: () => import('../favorites/favorites.module').then(m => m.FavoritesPageModule)
-//       },
-//       {
-//         path: 'categories',
-//         loadChildren: () => import('../categories/categories.module').then(m => m.CategoriesPageModule)
-//       },
-//       {
-//         path: 'shoppingList',
-//         loadChildren: () => import('../shoppingList/shoppingList.module').then(m => m.ShoppingListPageModule)
-//       },
-//       {
-//         path: 'all-recipes',
-//         loadChildren: () => import('../all-recipe/all-recipes.module').then(m => m.AllRecipesPageModule)
-//       },
-//       {
-//         path: 'edit-recipe',
-//         loadChildren: () => import('../edit-recipe/edit-recipe.module').then(m => m.EditRecipePageModule)
-//       },
-//       {
-//         path: 'add-recipes',
-//         loadChildren: () => import('../add-recipe/add-recipe.module').then(m => m.AddRecipePageModule)
-//       },
-//       {
-//         path: '',
-//         redirectTo: '/tabs/home',
-//         pathMatch: 'full'
-//       }
-//     ]
-//   },
-//   {
-//     path: '',
-//     redirectTo: '/tabs/home',
-//     pathMatch: 'full'
-//   }
-// ];
-
-// @NgModule({
-//   imports: [RouterModule.forChild(routes)],
-// })
-// export class TabsPageRoutingModule {}
