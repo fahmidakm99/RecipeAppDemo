e6296ed15a9422c3d623c620e06ff8828279afff
fahmidakm99 <93675075+fahmidakm99@users.noreply.github.com>
2025-03-18 08:11:55 +0530

    Update my-space.module.ts

--- src/app/my-space/my-space.module.ts
+++ src/app/my-space/my-space.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { MySpacePageRoutingModule } from './my-space-routing.module';
 
 import { MySpacePage } from './my-space.page';
+import { FilterModalComponent } from './filter-modal.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { MySpacePage } from './my-space.page';
     IonicModule,
     MySpacePageRoutingModule
   ],
-  declarations: [MySpacePage]
+  declarations: [MySpacePage, FilterModalComponent]
 })
 export class MySpacePageModule {}
